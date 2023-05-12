60d48b1f9480636104eda550ff69ed533d457c8b
Punithamani127 <132335831+Punithamani127@users.noreply.github.com>
2023-05-12 15:16:39 +0530

    Add files via upload

---
+++ chatbot.java
@@ -0,0 +1,33 @@
+package Programs;
+
+	import java.util.Scanner;
+
+	public class chatbot {
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        String input = "";
+	        
+	        System.out.println("Hi, I'm a chatbot. What's your name?");
+	        input = scanner.nextLine();
+	        System.out.println("Nice to meet you, " + input + ". How can I assist you today?");
+	        
+	        while (!input.equalsIgnoreCase("exit")) {
+	            input = scanner.nextLine();
+	            
+	            // Add your responses here based on user input
+	            if (input.contains("hello") || input.contains("hi")) {
+	                System.out.println("Hi there!");
+	            } else if (input.contains("how are you")) {
+	                System.out.println("I'm doing well, thank you.");
+	            } else if (input.contains("weather")) {
+	                System.out.println("I'm sorry, I am not capable of checking the weather at this moment.");
+	            } else {
+	                System.out.println("I'm sorry, I don't understand. Please try again or type 'exit' to end the chat.");
+	            }
+	        }
+	        
+	        System.out.println("Thank you for chatting with me. Goodbye!");
+	        scanner.close();
+	    }
+	}
+
